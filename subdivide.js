17a7f7cff06664516e6e673fc4fc7d4242b54d72
Lingo Chen <boeing.chen@gmail.com>
2022-06-19 14:14:44 +0800

    Add support for crease/sharpness Loops subdivision.
    Tags: Loop Subdivision Crease Sharpness

--- subdivide.js
+++ subdivide.js
@@ -815,10 +815,21 @@ const subdivideMB = (()=> {
 
 const subdivideLoop = (()=>{
 
+   /**
+    * @param {*} subd 
+    * @param {*} source 
+    * @param {*} computeSubdivideMid 
+    * 
+    * (E.1) New creased edge points – the midpoint Q of the old edge if sharpness >=1.0, or <0. 
+    * (E.2) New smooth edge points – the weighted average 1/4(3Q + R) when sharpness === 0;
+    * (E.3) New blended crease edge points – the linear interpolation of
+    *    point rules (E.1) and (E.2) with weight σ ∈ (0, 1),
+    */
    function refineEdge(subd, source, computeSubdivideMid) {
       const src = source.v.positionBuffer();
       const dest = subd.v.positionBuffer();
       const destV = subd.v;
+      const destH = subd.h;
       const hEdges = source.h;
       
       const midEdge = [0.0, 0.0, 0.0];
@@ -826,22 +837,48 @@ const subdivideLoop = (()=>{
 
       let offset = source.v.lengthPt();
       for (let [wEdge, left, right] of hEdges) {
-         // compute mid-Edge
          const leftV = hEdges.origin(left);
          const rightV = hEdges.origin(right);
-         const leftV1 = hEdges.origin( hEdges.prev(left) );
-         const rightV1 = hEdges.origin( hEdges.prev(right) );
+         let valence = 6;                    // no boundary, valence === 6
+         // get sharpness
+         let sharpness = hEdges.wSharpness(wEdge);
+         if ((sharpness < 0) || (sharpness >= 1)) {   // e1
+            if (sharpness >= 1) {
+               sharpness /= 2;
+            } else {
+               valence = 4;                  // boundary, valence == 4
+            }
+            // e1, crease mid-edge.
+            vec3.scale(midEdge, 0, src, leftV*3, 0.5);
+            vec3a.scaleAndAdd(midEdge, 0, src, rightV*3, 0.5);
+         } else { // e2, or e3
+            let q = 3/8, r = 1/8;   // e2
+            if (sharpness !== 0) {  // blend, e3
+               let u = 1.0 - sharpness;
+               q = q*u + (0.5*sharpness);
+               r = r*u;
+            }
+            sharpness /= 2;
+            // compute smooth, blend mid-Edge
+            const leftV1 = hEdges.origin( hEdges.prev(left) );
+            const rightV1 = hEdges.origin( hEdges.prev(right) );
          
-         vec3.scale(midEdge, 0, src, leftV*3, 3/8);
-         vec3a.scaleAndAdd(midEdge, 0, src, rightV*3, 3/8);
-         vec3a.scaleAndAdd(midEdge, 0, src, leftV1*3, 1/8);
-         vec3a.scaleAndAdd(midEdge, 0, src, rightV1*3, 1/8);
+            vec3.scale(midEdge, 0, src, leftV*3, q);
+            vec3a.scaleAndAdd(midEdge, 0, src, rightV*3, q);
+            vec3a.scaleAndAdd(midEdge, 0, src, leftV1*3, r);
+            vec3a.scaleAndAdd(midEdge, 0, src, rightV1*3, r);
+         }
+
          
-         // copy over
+         // copy over, midEdge
          vec3.copy(dest, (offset + wEdge) * 3, midEdge, 0); 
+         // copy over new sharpness
+         destH.setwSharpness(wEdge*2, sharpness);
+         destH.setwSharpness(wEdge*2+1, sharpness);
          // and setup outEdge pointer
          destV.setHalfEdge(offset+wEdge, computeSubdivideMid(left));
-         destV.setValence(offset+wEdge, 6);                         // newly create valence is always 6, unless opposite side is boundary.
+         destV.setValence(offset+wEdge, valence);
+         destV.setCrease(offset+wEdge, sharpness);
 
          
          // copy left(even) and right(odd) attribute, and compute new one.
@@ -862,6 +899,16 @@ const subdivideLoop = (()=>{
       }
    }
    
+   /**
+    * 
+    * @param {*} subd 
+    * @param {*} source 
+    * @param {*} computeSubdividehEdge 
+    * (V.1) New corner vertex points – the old vertex point V ,
+    * (V.2) New crease vertex points – the weighted average (3/4V + 1/8(a+b)) === 1/4(3V + S),
+    * (V.3) New smooth vertex points – the average (1 − nβn )V + βn n · T ,
+    * (v.4) New blended vertex points – the linear interpolation of point rules (V.2) and (V.3) with weight σ̄ ∈ (0, 1),
+    */
    function refineVertex(subd, source, computeSubdividehEdge) {
       const src = source.v.positionBuffer();
       const dest = subd.v.positionBuffer();
@@ -870,25 +917,55 @@ const subdivideLoop = (()=>{
 
       const pt = [0, 0, 0];
       // copy over and setup hEdge pointer
-      for (let vertex of source.v) {
-         // compute push down
-         
-         let beta = 3/16;
-         const k = source.v.valence(vertex);
-         if (k > 3) {
-            beta = 3 / (8*k);
+      for (let vertex of srcV) {
+         const valence = srcV.valence(vertex);
+         const crease = srcV.crease(vertex);
+         if (crease < 0) {             // corner, don't change
+            vec3.copy(dest, vertex*3, src, vertex*3);
+         } else if (crease >= 1) {     // crease
+            vec3.scale(pt, 0, src, vertex*3, 3/4);
+            for (let inEdge of srcV.inEdgeIter(vertex)) {
+               if (srchEdges.sharpness(inEdge) !== 0) {
+                  const out = srchEdges.origin(inEdge);
+                  vec3a.scaleAndAdd(pt, 0, src, out*3, 1/8);
+               }
+            }
+            vec3.copy(dest, vertex*3, pt, 0);
+         } else {
+            // compute push down
+            let beta = 3/16;
+            const k = valence;
+            if (k > 3) {
+               beta = 3 / (8*k);
+            }
+            // smooth or blend
+            if (crease === 0) {        // smooth
+               vec3.scale(pt, 0, src, vertex*3, 1 - k*beta);
+               for (let inEdge of srcV.inEdgeIter(vertex)) {
+                  const out = srchEdges.origin(inEdge);
+                  vec3a.scaleAndAdd(pt, 0, src, out*3, beta);
+               }
+            } else { // (0,1) blend between smooth and crease
+               const smooth = 1 - crease;
+               vec3.scale(pt, 0, src, vertex*3, smooth*(1 - k*beta) + crease*(3/4));
+               for (let inEdge of srcV.inEdgeIter(vertex)) {
+                  const out = srchEdges.origin(inEdge);
+                  if (srchEdges.sharpness(inEdge) !== 0) {
+                     vec3a.scaleAndAdd(pt, 0, src, out*3, beta * smooth + 1/8 * crease);
+                  } else {
+                     vec3a.scaleAndAdd(pt, 0, src, out*3, beta * smooth);
+                  }
+               }
+            }
+
+            vec3.copy(dest, vertex*3, pt, 0);
          }
-         vec3.scale(pt, 0, src, vertex*3, 1 - k*beta);
-         for (let inEdge of source.v.inEdgeIter(vertex)) {
-            let out = srchEdges.origin(inEdge);
-            vec3a.scaleAndAdd(pt, 0, src, out*3, beta);
-         }
-         
-         vec3.copy(dest, vertex*3, pt, 0);
+
 
          const hEdge = srcV.halfEdge(vertex);
          subd.v.setHalfEdge(vertex, computeSubdividehEdge(hEdge));
-         subd.v.setValence(vertex, srcV.valence(vertex));         // valence of the original vertex don't change
+         subd.v.setValence(vertex, valence);         // valence of the original vertex don't change
+         subd.v.setCrease(vertex, crease/2);
       }
    }
    
